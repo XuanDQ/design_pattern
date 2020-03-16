314a8afc588cf98762a9419a50a1627801458b18
XuanDQ <xuandq762@163.com>
2020-03-16 17:08:26 +0800

    // 提交建造者模式demo

---
+++ src/main/java/com/imooc/design/pattern/creational/builder/CourseActualBuilder.java
@@ -0,0 +1,40 @@
+package com.imooc.design.pattern.creational.builder;
+
+/**
+ * @Author safeXuan
+ * @Version 1.0 2020/3/16
+ */
+public class CourseActualBuilder extends CourseBuilder {
+
+    private Course course = new Course();
+
+    @Override
+    public void buildCourseName(String courseName) {
+        course.setCourseName(courseName);
+    }
+
+    @Override
+    public void buildCoursePPT(String coursePPT) {
+        course.setCoursePPT(coursePPT);
+    }
+
+    @Override
+    public void buildCourseVideo(String courseVideo) {
+        course.setCourseVideo(courseVideo);
+    }
+
+    @Override
+    public void buildCourseArtical(String courseArtical) {
+        course.setCourseArtical(courseArtical);
+    }
+
+    @Override
+    public void buildCourseQA(String courseQA) {
+        course.setCourseQA(courseQA);
+    }
+
+    @Override
+    public Course makeCourse() {
+        return course ;
+    }
+}
---
+++ src/main/java/com/imooc/design/pattern/creational/builder/Test.java
@@ -0,0 +1,21 @@
+package com.imooc.design.pattern.creational.builder;
+
+/**
+ * @Author safeXuan
+ * @Version 1.0 2020/3/16
+ */
+public class Test {
+    public static void main(String[] args) {
+        Coach coach = new Coach();
+        CourseBuilder courseBuilder = new CourseActualBuilder();
+
+        coach.setCourseBuilder(courseBuilder);
+
+        Course course = coach.makeCourse("Java设计模式精讲",
+                "Java设计模式精讲PPT",
+                "Java设计模式精讲视频",
+                "Java设计模式精讲手记",
+                "Java设计模式精讲问答");
+        System.out.println(course);
+    }
+}
